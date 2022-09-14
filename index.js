4f239cdba046800ab3c33983cfb8b85fc68544cf
pavanteja07 <anonymouscadate07@gmail.com>
2022-09-14 21:37:41 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+var http = require('http');
+
+// Creating a server
+
+http.createServer(function (req, res) {
+
+// Write response as Html(text)
+
+res.writeHead(200, {'Content-Type': 'text/html'});
+
+// Writing static text
+
+res.end('"Hello Node.js!"');
+
+// Server listening on port number 4200
+
+}).listen(4200);
